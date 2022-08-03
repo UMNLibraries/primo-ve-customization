c25c65736b5509b1e30aa5cb155a369e51c48c3e
Jeff Peterson <gpeterso@umn.edu>
2022-08-03 16:42:26 -0500

    Update dev server proxy
    Add redirect for SAML authentication.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 import ZipPlugin from 'zip-webpack-plugin';
 
+const CUST_PATH = '/discovery/custom';
+//const PROXY_TARGET = 'https://umn-psb.primo.exlibrisgroup.com';
+const PROXY_TARGET = 'https://umn-psb.alma.exlibrisgroup.com';
+
 const baseConfig = {
   entry: {
     '01UMN_INST-TWINCITIES': './src/TWINCITIES/js/index.js',
@@ -7,43 +11,23 @@ const baseConfig = {
   },
   output: {
     filename: '[name]/js/custom.js',
-    publicPath: '/discovery/custom',
+    publicPath: CUST_PATH,
     clean: true,
   },
   devServer: {
-//    proxy: [
-//      {
-//        context: ['/discovery/**', '!/discovery/custom/**/*.js', '/primaws/rest/**'],
-//        target: 'https://umn-psb.primo.exlibrisgroup.com',
-//        changeOrigin: true,
-//        secure: false,
-//      },
-//    ],
-//    proxy: {
-//      context: () => true, // proxy everything
-//      target: 'https://umn-psb.primo.exlibrisgroup.com',
-//      changeOrigin: true,
-//      bypass: (req, res, proxyOptions) => {
-//        if (req.path.startsWith('/discovery/custom')) {
-//          console.log(req.headers);
-//          return `http://localhost:8080/${req.path}`;
-//        }
-//      },
-//    },
-    devMiddleware: {
-      index: false,
-      publicPath: '/discovery/custom',
-    },
     proxy: [
-//      {
-//        context: '/discovery/custom/**',
-//        target: 'http:localhost:8080',
-//        secure: false,
-//        pathRewrite: (path, req) => path.replace('/discovery/custom/', '/'),
-//      },
       {
-        context: (path) => !path.startsWith('/discovery/custom'),
-        target: 'https://umn-psb.primo.exlibrisgroup.com',
+        context: ['/primaws/suprimaLogin', '/primaws/suprimaExtLogin'],
+        target: PROXY_TARGET,
+        changeOrigin: true, 
+        followRedirects: true,
+        onProxyReq(proxyReq, req, res) {
+          res.writeHead(302, { location: PROXY_TARGET + req.url });
+        },
+      },
+      {
+        context: (path) => !path.startsWith(CUST_PATH),
+        target: PROXY_TARGET,
         changeOrigin: true,
       }
     ]
