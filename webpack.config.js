79abd4cd1edecb438a9a1126ac2f974369bac013
Jeff Peterson <gpeterso@umn.edu>
2022-08-03 14:23:03 -0500

    Add devServer config

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,51 @@ import ZipPlugin from 'zip-webpack-plugin';
 
 const baseConfig = {
   entry: {
-    TWINCITIES: './src/index.js',
-    CENTRAL_PACKAGE: './src/central-package.js',
+    '01UMN_INST-TWINCITIES': './src/index.js',
+    '01UMN_INST-CENTRAL_PACKAGE': './src/central-package.js',
   },
   output: {
     filename: '[name]/js/custom.js',
     clean: true,
+    publicPath: '/discovery/custom'
+  },
+  devServer: {
+//    proxy: [
+//      {
+//        context: ['/discovery/**', '!/discovery/custom/**/*.js', '/primaws/rest/**'],
+//        target: 'https://umn-psb.primo.exlibrisgroup.com',
+//        changeOrigin: true,
+//        secure: false,
+//      },
+//    ],
+//    proxy: {
+//      context: () => true, // proxy everything
+//      target: 'https://umn-psb.primo.exlibrisgroup.com',
+//      changeOrigin: true,
+//      bypass: (req, res, proxyOptions) => {
+//        if (req.path.startsWith('/discovery/custom')) {
+//          console.log(req.headers);
+//          return `http://localhost:8080/${req.path}`;
+//        }
+//      },
+//    },
+    devMiddleware: {
+      index: false,
+      publicPath: '/discovery/custom',
+    },
+    proxy: [
+//      {
+//        context: '/discovery/custom/**',
+//        target: 'http:localhost:8080',
+//        secure: false,
+//        pathRewrite: (path, req) => path.replace('/discovery/custom/', '/'),
+//      },
+      {
+        context: (path) => !path.startsWith('/discovery/custom'),
+        target: 'https://umn-psb.primo.exlibrisgroup.com',
+        changeOrigin: true,
+      }
+    ]
   },
 };
 
