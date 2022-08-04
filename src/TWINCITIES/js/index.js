cd4373dc0ca97596738e9dbdc6c362dec1f6a409
Jeff Peterson <gpeterso@umn.edu>
2022-08-04 11:40:49 -0500

    Cleanup

--- src/TWINCITIES/js/index.js
+++ src/TWINCITIES/js/index.js
@@ -3,38 +3,21 @@
 import '../css/foo.css';
 import '../css/bar.css';
 
-
-/**
- * I wonder if we could host host the central pkg javascript & css in a VE view
- * named "CENTRAL_PACKAGE"...
- *
- */
-const centralPackageUrl = '/discovery/custom/01UMN_INST-CENTRAL_PACKAGE/js/custom.js';
-
-
 //TODO: maybe use SystemJS for this?
 // (might need to polyfill Promise or just use a onload callback instead) 
-const loadScript = () => {
+const loadCentralPackage = () => {
   return new Promise((resolve, reject) => {
     const script = document.createElement('script');
-    document.head.appendChild(script);
+    const centralPackageUrl = 
+      '/discovery/custom/01UMN_INST-CENTRAL_PACKAGE/js/custom.js';
     script.onload = resolve;
     script.onerror = reject;
     script.async = true;
     script.src = centralPackageUrl;
+    document.head.appendChild(script);
   });
 };
 
-loadScript().then(() => 
-  angular.module('viewCustom', ['centralCustom']).run(() => console.log('LOCAL')));
-
-/*
-// this might not work without bootstrapping...
-angular.module('main', ['angularLoad'])
-  .run(['angularLoad', angularLoad => {
-    angularLoad.loadScript(centralPackageUrl).then(() => {
-      angular.module('viewCustom', ['centralCustom']);
-    })
-  }]);
-*/
+loadCentralPackage().then(() => 
+  angular.module('viewCustom', ['centralCustom']));
 
