f952198fc47b1a84a33ea6b82c7092d6054bc214
Jeff Peterson <gpeterso@umn.edu>
2022-10-12 15:30:17 -0500

    Add empty css for TWINCITIES view

--- src/packages/01UMN_INST-TWINCITIES/index.ts
+++ src/packages/01UMN_INST-TWINCITIES/index.ts
@@ -1,10 +1,8 @@
 import 'promise-polyfill/src/polyfill';
 
 // NOTE for future: lodash (_) may be available globally 
-//
-import './css/foo.css';
-//import './css/bar.css';
 
+import './css/empty.css';
 import { loadCentralPackage } from '../../util/load';
 
 loadCentralPackage().then(() => 
