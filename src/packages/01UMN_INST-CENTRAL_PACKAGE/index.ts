62f27f603eedd78405504bec36e1de204646bded
Jeff Peterson <gpeterso@umn.edu>
2022-10-17 17:15:55 -0500

    Add custom filter module

--- src/packages/01UMN_INST-CENTRAL_PACKAGE/index.ts
+++ src/packages/01UMN_INST-CENTRAL_PACKAGE/index.ts
@@ -1,29 +1,17 @@
-
 if (location.search.includes('vid=01UMN_INST:CENTRAL_PACKAGE')) {
   location.href = location.toString().replace('vid=01UMN_INST:CENTRAL_PACKAGE', 
                                               'vid=01UMN_INST:TWINCITIES');
 }
 
-import globals from './globals';
-
-console.log(globals);
+//import globals from './globals';
 
 import './color-theme/colors.css';
 import Components from './components/components.module';
+import Filters from './filters/filters.module';
 
-class GreeterService {
-  public static $inject = ['$log'];
 
-  constructor(private $log: ng.ILogService) {
-    this.$log = $log;
-  }
-
-  init() {
-    this.$log.info('HELLO from central package!');
-  }
-}
-
-angular.module('centralCustom', [Components])
-  .service('greeterService', GreeterService)
-  .run(['greeterService', (greeterService: GreeterService) => greeterService.init()]);
+angular.module('centralCustom', [
+  Components, 
+  Filters,
+]);
 
