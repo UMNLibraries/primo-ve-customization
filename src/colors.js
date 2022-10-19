e5a8fa6a3e429372dc3dc05772186916631552db
Jeff Peterson <gpeterso@umn.edu>
2022-10-19 16:38:41 -0500

    Update common colors
    Note intended use...

--- src/colors.js
+++ src/colors.js
@@ -3,6 +3,11 @@ import { readFile } from 'fs/promises';
 const jsonFile = './packages/01UMN_INST-CENTRAL_PACKAGE/color-theme/colors.json';
 const colorTheme = await readFile(new URL(jsonFile, import.meta.url)).then(JSON.parse);
 
+/**
+ * Defines a common set of color variables that can be shared between
+ * JS/TS and Sass/SCSS. Includes Primo's built-in color theme variables 
+ * (defined in colors.json) and any additional custom color variables.
+ */
 const colors = {
   ...colorTheme,
   lightSecondary: '#ffd75f', // light gold from the Nokomis theme
