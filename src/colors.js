25b3b71ec0889978ec1bf0aa4d1268a6327f11a3
Jeff Peterson <gpeterso@umn.edu>
2022-10-20 09:33:45 -0500

    Update colors module

--- src/colors.js
+++ src/colors.js
@@ -1,22 +1,27 @@
 import { readFile } from 'fs/promises';
 
-const jsonFile = './packages/01UMN_INST-CENTRAL_PACKAGE/color-theme/colors.json';
-const colorTheme = await readFile(new URL(jsonFile, import.meta.url)).then(JSON.parse);
+const convertPropPair = (scss, pair) =>
+  scss + `$${pair.join(': ')};`;
+
+const convertable = {
+  toScss: function() {
+    return Object.entries(this).reduce(convertPropPair, '');
+  }
+}
+
+const colorTheme = await readFile(
+  new URL(
+    './packages/01UMN_INST-CENTRAL_PACKAGE/color-theme/colors.json', 
+    import.meta.url)
+).then(JSON.parse);
 
 /**
  * Defines a common set of color variables that can be shared between
  * JS/TS and Sass/SCSS. Includes Primo's built-in color theme variables 
  * (defined in colors.json) and any additional custom color variables.
  */
-const colors = {
+export default {
+  __proto__: convertable,
   ...colorTheme,
   lightSecondary: '#ffd75f', // light gold from the Nokomis theme
 }
-
-colors.toScss = function() {
-  return Object.entries(this)
-    .filter(entry => typeof (entry[1]) === 'string')
-    .reduce((scss, entry) => scss += `$${entry.join(': ')};`, '');
-}
-
-export default colors;
