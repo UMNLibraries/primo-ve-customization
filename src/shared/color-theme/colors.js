404c99af50c62e6f3696d645dc30d1100f604847
Jeff Peterson <gpeterso@umn.edu>
2022-10-26 17:34:48 -0500

    Fix formatting

--- src/shared/color-theme/colors.js
+++ src/shared/color-theme/colors.js
@@ -1,24 +1,25 @@
-import { readFile } from 'fs/promises';
+import { readFile } from "fs/promises";
 
-const colorTheme = 
-  await readFile(new URL('./colors.json', import.meta.url))
-  .then(JSON.parse);
+const colorTheme = await readFile(
+  new URL("./colors.json", import.meta.url)
+).then(JSON.parse);
 
 const convertable = {
-  toSass: function() {
+  toSass: function () {
     return Object.entries(this).reduce(
-      (sass, entry) => sass + `$${entry.join(': ')};`, 
-      '');
-  }
-}
+      (sass, entry) => sass + `$${entry.join(": ")};`,
+      ""
+    );
+  },
+};
 
 /**
  * Defines a common set of color variables that can be shared between
- * JS/TS and Sass/SCSS. Includes Primo's built-in color theme variables 
+ * JS/TS and Sass/SCSS. Includes Primo's built-in color theme variables
  * (defined in colors.json) and any additional custom color variables.
  */
 export default {
   __proto__: convertable,
   ...colorTheme,
-  lightSecondary: '#ffd75f', // light gold from the Nokomis theme
-}
+  lightSecondary: "#ffd75f", // light gold from the Nokomis theme
+};
--- src/shared/filters/uniq-by-keys.filter.ts
+++ src/shared/filters/uniq-by-keys.filter.ts
@@ -1,15 +1,15 @@
 /**
- * Custom filter that removes duplicate objects from a list based on 
+ * Custom filter that removes duplicate objects from a list based on
  * 1 or more matching properties.
- * 
+ *
  * @param input List of objects to de-duplicate
  * @param keys List of object properties to match
- * 
+ *
  * TEMPLATE USAGE EXAMPLE
  * {{ users | uniqByKeys:['firstName', 'lastName'] }}
- * 
+ *
  */
-const uniqByKeys = () => 
-  (input: Array<object>, keys: Array<any>) => _.uniqBy(input, (obj: any) => keys.map(key => obj[key]).join());
+const uniqByKeys = () => (input: Array<object>, keys: Array<any>) =>
+  _.uniqBy(input, (obj: any) => keys.map((key) => obj[key]).join());
 
 export default uniqByKeys;
