2d0093c74b6c9b892056f5ef7f12c5c219724e21
Jeff Peterson <gpeterso@umn.edu>
2022-10-26 16:12:31 -0500

    Remove promise polyfill
    No longer needed if we're not downloading the central package at
    runtime.

--- src/views/01UMN_INST-TWINCITIES/index.ts
+++ src/views/01UMN_INST-TWINCITIES/index.ts
@@ -1,15 +1,3 @@
-import 'promise-polyfill/src/polyfill';
-
-//import { loadCentralPackage } from '../../util/load';
-
-/*
-loadCentralPackage().then(() => 
-  angular.module('viewCustom', ['centralCustom']));
-
- */
-
-
 import Shared from '../../shared/index';
-//angular.module('viewCustom', ['centralCustom']);
-angular.module('viewCustom', [Shared]);
 
+angular.module('viewCustom', [Shared]);
