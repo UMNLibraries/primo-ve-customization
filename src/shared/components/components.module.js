7c4c630cecdebbe5db9b380fa2782b4274a8ace4
Jeff Peterson <gpeterso@umn.edu>
2022-11-02 15:10:32 -0500

    Add advanced search customization

--- src/shared/components/components.module.js
+++ src/shared/components/components.module.js
@@ -1,7 +1,9 @@
 import Icon from './icon/icon.module';
+import Search from './search/search.module';
 
 export default angular
   .module('components', [
     Icon,
+    Search,
   ])
   .name;
---
+++ src/shared/components/search/search.module.js
@@ -0,0 +1,13 @@
+//import FullView from './full-view/full-view.module';
+import SearchBar from './search-bar/search-bar.module';
+//import SearchResult from './search-result/search-result.module';
+//import Topbar from './topbar/topbar.module';
+
+export default angular
+  .module('search', [
+    //FullView,
+    SearchBar,
+    //SearchResult,
+    //Topbar,
+  ])
+  .name;
