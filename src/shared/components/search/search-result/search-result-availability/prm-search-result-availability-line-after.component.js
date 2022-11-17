8787fbb1e0ae631d10790f0a54cecd554269723c
Jeff Peterson <gpeterso@umn.edu>
2022-11-17 08:21:09 -0600

    Add HathiTrust plugin css

--- src/shared/components/search/search-result/search-result-availability/prm-search-result-availability-line-after.component.js
+++ src/shared/components/search/search-result/search-result-availability/prm-search-result-availability-line-after.component.js
@@ -1,5 +1,6 @@
-import template from './prm-search-result-availability-line-after.html';
+import template from "./prm-search-result-availability-line-after.html";
+import "primo-explore-hathitrust-availability/css/style.css";
 
 export default {
-  template: template
+  template: template,
 };
