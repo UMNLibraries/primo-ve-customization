3d98fb271a332c25f7c3092eca3209a3cf9b1379
Jeff Peterson <gpeterso@umn.edu>
2022-11-28 13:43:35 -0600

    Add skip-to component

--- src/shared/components/search/search.module.js
+++ src/shared/components/search/search.module.js
@@ -1,11 +1,11 @@
 //import FullView from './full-view/full-view.module';
 import SearchBar from "./search-bar/search-bar.module";
 import SearchResult from "./search-result/search-result.module";
-//import Topbar from './topbar/topbar.module';
+import Topbar from "./topbar/topbar.module";
 
 export default angular.module("search", [
   //FullView,
   SearchBar,
   SearchResult,
-  //Topbar,
+  Topbar,
 ]).name;
---
+++ src/shared/components/search/topbar/topbar.module.js
@@ -0,0 +1,5 @@
+import PrmSkipToAfter from "./skip-to/prm-skip-to-after.component";
+
+export default angular
+  .module("topbar", [])
+  .component("prmSkipToAfter", PrmSkipToAfter).name;
