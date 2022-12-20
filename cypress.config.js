4c59d8de8168ebf4b628508c0daacaf8e91ade27
Jeff Peterson <gpeterso@umn.edu>
2022-12-20 11:19:05 -0600

    Increase cypress timeout
    In some cases (e.g. cold cache) Primo VE can be pretty slow at first...

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080/discovery",
 
+    defaultCommandTimeout: 12000,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
