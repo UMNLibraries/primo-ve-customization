f39f62fba6a267f6544560f33d3328bf0728499c
Jeff Peterson <gpeterso@umn.edu>
2022-12-21 14:50:57 -0600

    Refactor qpoint test

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,27 +1,20 @@
-import { ViewCode } from "../support/view-code";
+import { SearchPage } from "../pages/search";
 
 describe("Search", () => {
+  let searchPage: SearchPage;
+
   context("Twin Cities", () => {
     before(() => {
-      cy.visit(`/search?vid=${ViewCode.TWINCITIES}`);
+      searchPage = new SearchPage("01UMN_INST:TWINCITIES");
+      searchPage.visit();
     });
 
-    describe("Chat", () => {
+    describe("QuestionPoint Chat", () => {
       it("displays a chat dialog when clicked", () => {
-        cy.get("a[show-chat-on-click]").click();
-        cy.get(".qpoint-chat iframe")
-          .its("0.contentDocument.body")
-          .should("not.be.empty")
-          .then(cy.wrap)
-          .find("iframe") // the chat widget is in a weird nested iframe
-          .its("0.contentDocument.body")
-          .should("not.be.empty")
-          .then(cy.wrap)
-          .contains("h2", /libraries.*chat/i);
-
-        //close the dialog
-        cy.get(".qpoint-chat").find("md-dialog-actions button").click();
-        cy.get(".qpoint-chat").should("not.exist");
+        searchPage.chatLink.click();
+        searchPage.chatDialog.contains("h2", /libraries.*chat/i);
+        searchPage.chatCloseButton.click();
+        searchPage.chatDialog.should("not.exist");
       });
     });
   });
--- cypress/pages/search.ts
+++ cypress/pages/search.ts
@@ -8,4 +8,27 @@ export class SearchPage extends PrimoPage {
   searchFor(query: string) {
     return cy.get("#searchBar").type(query + "{enter}");
   }
+
+  get chatLink() {
+    return cy.get("a[show-chat-on-click]");
+  }
+
+  /**
+   * returns the questionpoint chat widget (nested iframe)
+   */
+  get chatDialog() {
+    return cy
+      .get(".qpoint-chat iframe")
+      .its("0.contentDocument.body")
+      .should("not.be.empty")
+      .then(cy.wrap)
+      .find("iframe") // the chat widget is in a weird nested iframe
+      .its("0.contentDocument.body")
+      .should("not.be.empty")
+      .then(cy.wrap);
+  }
+
+  get chatCloseButton() {
+    return cy.get(".qpoint-chat").find("md-dialog-actions button");
+  }
 }
