738d307231068fdbefcc36e146cdde519e391205
Jeff Peterson <gpeterso@umn.edu>
2022-12-27 13:09:32 -0600

    Add basic browzine e2e test

---
+++ cypress/e2e/fulldisplay.cy.ts
@@ -0,0 +1,24 @@
+import { FullDisplayPage } from "../pages";
+import { inAllViews } from "../support/e2e";
+
+describe("Full Display", () => {
+  let page: FullDisplayPage;
+
+  inAllViews((view) => {
+    describe("LibKey", () => {
+      const articleId =
+        "cdi_pubmedcentral_primary_oai_pubmedcentral_nih_gov_4310632";
+
+      beforeEach(() => {
+        page = new FullDisplayPage(view, articleId);
+        page.visit();
+      });
+
+      it("displays a PDF download link", () => {
+        cy.get("a.browzine-direct-to-pdf-link", { timeout: 20_000 }).should(
+          "exist"
+        );
+      });
+    });
+  });
+});
