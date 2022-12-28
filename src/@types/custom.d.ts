8b94233f3c8c0e898247e1d8947c6a514deb4453
Jeff Peterson <gpeterso@umn.edu>
2022-12-28 13:27:56 -0600

    Add custom & global type def files

---
+++ src/@types/custom.d.ts
@@ -0,0 +1,9 @@
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.html" {
+  const content: string;
+  export default content;
+}
---
+++ src/@types/index.d.ts
@@ -0,0 +1,6 @@
+declare global {
+  const angular: ng.IAngularStatic;
+  const _: _.LoDashStatic;
+}
+
+export {};
