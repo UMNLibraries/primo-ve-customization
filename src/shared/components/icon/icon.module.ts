fed99c409a2a434b88ea13cbb3c75d4905b574ea
Jeff Peterson <gpeterso@umn.edu>
2023-01-04 15:27:57 -0600

    Convert icon component to TS

--- src/shared/components/icon/icon.module.ts
+++ src/shared/components/icon/icon.module.ts
@@ -2,12 +2,12 @@ import { PrmIconAfterComponent } from "./prm-icon-after.component";
 import toggle from "./svg-sprite-toggle.svg";
 import "./icon.scss";
 
+config.$inject = ["$mdIconProvider"];
+function config($mdIconProvider: ng.material.IIconProvider) {
+  $mdIconProvider.iconSet("toggle", toggle, 24);
+}
+
 export const IconModule = angular
   .module("icon", [])
   .component("prmIconAfter", PrmIconAfterComponent)
-  .config([
-    "$mdIconProvider",
-    ($mdIconProvider) => {
-      $mdIconProvider.iconSet("toggle", toggle, 24);
-    },
-  ]).name;
+  .config(config).name;
