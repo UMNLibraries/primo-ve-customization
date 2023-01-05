19a4841b923dffc8b30f0892ff6c197a61d82d15
Jeff Peterson <gpeterso@umn.edu>
2023-01-05 16:21:20 -0600

    Convert TC no-results component to TS

--- src/views/01UMN_INST-TWINCITIES/components/no-results/no-results.component.ts
+++ src/views/01UMN_INST-TWINCITIES/components/no-results/no-results.component.ts
@@ -2,6 +2,8 @@ import "./no-results.scss";
 import template from "./no-results.html";
 
 class NoResultsController {
+  private prmSearchResultListCtrl: ng.IComponentController;
+
   get searchString() {
     return this.prmSearchResultListCtrl.searchString;
   }
