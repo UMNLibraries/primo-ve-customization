ff71f1e47bdcd28fc3e242fbee07aea4f4bfa414
Jeff Peterson <gpeterso@umn.edu>
2023-01-05 13:01:34 -0600

    Update custom asset types
    - add png
    - declare svg as string (vs any)

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -1,5 +1,10 @@
 declare module "*.svg" {
-  const content: any;
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
   export default content;
 }
 
