58b3b166b2ad0fc87af490bf2d75f81f047cce56
Jeff Peterson <gpeterso@umn.edu>
2023-01-10 15:09:11 -0600

    Add full-view style

--- src/shared/components/search/full-view/full-view.module.ts
+++ src/shared/components/search/full-view/full-view.module.ts
@@ -1,3 +1,6 @@
+import "./full-view.scss";
+import "./recommendations.scss";
+
 import { GetItModule } from "./get-it";
 
 export const FullViewModule = angular.module("fullView", [GetItModule]).name;
