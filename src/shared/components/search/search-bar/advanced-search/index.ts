63c08f25f7a08e8eaa5cd81a94d156689dc892df
Jeff Peterson <gpeterso@umn.edu>
2023-01-11 17:00:54 -0600

    Update advanced search component
    - move to a separate directory
    - add style

---
+++ src/shared/components/search/search-bar/advanced-search/index.ts
@@ -0,0 +1,4 @@
+import "./advanced-search.scss";
+
+export * from "./prm-advanced-search-after.component";
+export * from "./uncollapse-advanced-search.component";
--- src/shared/components/search/search-bar/search-bar.module.ts
+++ src/shared/components/search/search-bar/search-bar.module.ts
@@ -1,6 +1,6 @@
 //import { PrmSearchBarAfterComponent } from "./prm-search-bar-after.component";
-import { PrmAdvncedSearchAfterComponent } from "./prm-advanced-search-after.component";
-import { UncollapseAdvancedSearchComponent } from "./uncollapse-advanced-search.component";
+import { PrmAdvncedSearchAfterComponent } from "./advanced-search";
+import { UncollapseAdvancedSearchComponent } from "./advanced-search";
 
 export const SearchBarModule = angular
   .module("searchBar", [])
