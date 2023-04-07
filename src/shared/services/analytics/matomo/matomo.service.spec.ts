3bbcd540c2c31d8a9495ba7c166937db2dbaf1f2
Jeff Peterson <gpeterso@umn.edu>
2023-04-07 10:24:14 -0500

    Add Matomo service
    Not enabled at the moment...

---
+++ src/shared/services/analytics/matomo/matomo.service.spec.ts
@@ -0,0 +1,44 @@
+import { Matomo } from "./matomo.service";
+
+const view = "TEST";
+
+describe("Matomo Service", () => {
+  let $rootScope: ng.IRootScopeService,
+    $location: ng.ILocationService,
+    $window: ng.IWindowService,
+    matomo: Matomo;
+
+  beforeEach(
+    angular.mock.inject(($injector) => {
+      $rootScope = $injector.get("$rootScope");
+      $location = $injector.get("$location");
+      $window = $injector.get("$window");
+      matomo = new Matomo($rootScope, $location, $window, view);
+    })
+  );
+
+  it("should create a global _paq array", () => {
+    expect($window._paq).toBeDefined();
+  });
+
+  it("should record pageview events when the location changes", () => {
+    let url = "/foo";
+    matomo.trackPageviews();
+    $rootScope.$broadcast("$locationChangeSuccess", url);
+    const pageViewEvents = $window._paq.filter((e: string[]) =>
+      e.includes("trackPageView")
+    );
+    // should have initial page view event and location change event
+    expect(pageViewEvents).toHaveSize(2);
+  });
+
+  it("should push event tracking data to global _paq array", () => {
+    matomo.trackEvent("Links", "My Link Click");
+    expect($window._paq).toContain([
+      "trackEvent",
+      "Links",
+      "My Link Click",
+      undefined,
+    ]);
+  });
+});
---
+++ src/shared/services/analytics/matomo/matomo.service.ts
@@ -0,0 +1,53 @@
+import { Analytics } from "../analytics";
+
+const PROD_HOST_PATTERN = /^(primo.lib|umn.primo|umn.alma).*/;
+
+function loadAnalytics(window: ng.IWindowService) {
+  var _paq = (window._paq = window._paq || []);
+  (function () {
+    var u = "https://apps.lib.umn.edu/matomo/";
+    _paq.push(["setTrackerUrl", u + "matomo.php"]);
+    _paq.push(["setSiteId", "11"]);
+    var d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0];
+    g.async = true;
+    g.src = u + "matomo.js";
+    s.parentNode.insertBefore(g, s);
+  })();
+}
+
+export class Matomo implements Analytics {
+  static $inject = ["$rootScope", "$location", "$window", "view"];
+  constructor(
+    private $rootScope: ng.IRootScopeService,
+    private $location: ng.ILocationService,
+    private $window: ng.IWindowService,
+    private view: string
+  ) {
+    this.$window._paq = [];
+    if (PROD_HOST_PATTERN.test(this.$location.host())) {
+      loadAnalytics(this.$window);
+    }
+  }
+
+  private get _paq(): Array<Array<string | Function>> {
+    return this.$window._paq;
+  }
+
+  trackPageviews(): void {
+    this._paq.push(["trackPageView"]);
+    this._paq.push(["enableLinkTracking"]);
+    this.$rootScope.$on(
+      "$locationChangeSuccess",
+      (_event, _newUrl, _oldUrl) => {
+        this.$window._paq.push(["trackPageView"]);
+      }
+    );
+  }
+
+  trackEvent(category: string, action: string, label?: string): void {
+    //this.$window.ga("send", "event", category, action, label);
+    this._paq.push(["trackEvent", category, action, label]);
+  }
+}
