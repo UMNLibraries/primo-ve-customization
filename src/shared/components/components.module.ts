2ba313f4382c1a33510f696a36829b13ed635eac
Jeff Peterson <gpeterso@umn.edu>
2023-04-13 19:02:41 -0500

    Remove custom image module
    No longer needed. Using the undocumented custom-ui.svg instead.

--- src/shared/components/components.module.ts
+++ src/shared/components/components.module.ts
@@ -1,14 +1,13 @@
 import "./primo-explore.scss";
 import "./report-problem.scss";
+import "./icon.scss";
 
-import { IconModule } from "./icon";
 import { SearchModule } from "./search";
 import { AccountModule } from "./account";
 import { CollectionDiscoveryModule } from "./collection-discovery";
 
 export const ComponentsModule = angular.module("components", [
   AccountModule,
-  IconModule,
   SearchModule,
   CollectionDiscoveryModule,
 ]).name;
