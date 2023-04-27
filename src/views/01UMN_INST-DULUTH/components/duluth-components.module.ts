bd1d9470eb2adee8eb9d551e27753855adaa1e50
Jeff Peterson <gpeterso@umn.edu>
2023-04-27 11:28:10 -0500

    Add Duluth no-results component
    Requested by Nichole (TDX #1166730)

--- src/views/01UMN_INST-DULUTH/components/duluth-components.module.ts
+++ src/views/01UMN_INST-DULUTH/components/duluth-components.module.ts
@@ -1,9 +1,14 @@
 import { AskusBubbleComponent } from "./askus-bubble";
+import { NoResultsComponent } from "./no-results";
 import { FooterModule } from "./footer";
 
 export const DuluthComponentsModule = angular
   .module("duluthComponents", [FooterModule])
   .component("askusBubble", AskusBubbleComponent)
+  .component("noResults", NoResultsComponent)
+  .component("prmNoSearchResultAfter", {
+    template: "<no-results></no-results>",
+  })
   .component("prmSearchBookmarkFilterAfter", {
     template: "<askus-bubble></askus-bubble>",
   }).name;
