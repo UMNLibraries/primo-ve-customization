892c87c08c80019dd096ae061adc5ee4c9256eb4
Jeff Peterson <gpeterso@umn.edu>
2024-10-03 14:16:01 -0500

    Replace Duluth ask-us bubble
    ... with a generic help icon like Morris and Crookson.

--- src/views/01UMN_INST-DULUTH/components/duluth-components.module.ts
+++ src/views/01UMN_INST-DULUTH/components/duluth-components.module.ts
@@ -1,14 +1,14 @@
-import { AskusBubbleComponent } from "./askus-bubble";
+import { AskusButtonComponent } from "./askus-button";
 import { NoResultsComponent } from "./no-results";
 import { FooterModule } from "./footer";
 
 export const DuluthComponentsModule = angular
   .module("duluthComponents", [FooterModule])
-  .component("askusBubble", AskusBubbleComponent)
+  .component("askusButton", AskusButtonComponent)
   .component("noResults", NoResultsComponent)
   .component("prmNoSearchResultAfter", {
     template: "<no-results></no-results>",
   })
   .component("prmSearchBookmarkFilterAfter", {
-    template: "<askus-bubble></askus-bubble>",
+    template: "<askus-button></askus-button>",
   }).name;
